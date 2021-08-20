3f215fb452ad2e0f7246e6f239726aefe176fa81
Nelson Maldonado <play2nino@gmail.com>
2021-08-19 23:59:40 -0700

    saved

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -20,10 +20,70 @@ const initialFormErrors = {
   password: "",
 }
 
+const initialUser = []
+const initialDisabled = true
+
 function App() {
+  const [user, setUser] = useState(initialUser)
+  const [formValues, setFormValues] = useState(initialFormValues)
+  const [formErrors, setFormErrors] = useState(initialDisabled)
+  const [disabled, setDisabled] = useState(initialDisabled)
+
+  const getUser = () => {
+    axios
+      .get("https://reqres.in/api/users")
+      .then((response) => {
+        console.log(`This is the res from API`, { response })
+        setUser(response.data.data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+
+  const postNewUser = (newUser) => {
+    axios
+      .post("https://reqres.in/api/users", newUser)
+      .then((res) => {
+        console.log(res)
+        setUser([res.data.data, ...user])
+      })
+      .catch((err) => console.log(err))
+    setFormValues(initialFormValues)
+  }
+
+  const validate = (name, value) => {
+    yup
+      .reach(schema, name)
+      .validate(value)
+      .then(() => setFormErrors({ ...formErrors, [name]: "" }))
+      .catch((err) => setFormErrors({ ...formErrors, [name]: err.errors[0] }))
+  }
+
+  const inputChange = (name, value) => {
+    validate(name, value)
+    setFormValues({ ...formValues, [name]: value })
+  }
+
+  const formSubmit = () => {
+    const newUser = {
+      name: formValues.name.trim(),
+      email: formValues.email.trim(),
+      password: formValues.password.trim(),
+      // terms: false,     << Finish this line
+    }
+    postNewUser(newUser)
+  }
+
   return (
     <div className="App">
-      <OnForm />
+      <OnForm
+        values={formValues}
+        change={inputChange}
+        submit={formSubmit}
+        disabled={disabled}
+        errors={formErrors}
+      />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Form App by Nelson Maldonado</p>
--- user-onboarding/src/component/Form.js
+++ user-onboarding/src/component/Form.js
@@ -18,6 +18,66 @@ export default function OnForm(props) {
     <form className="form container" onSubmit={onSubmit}>
       <div className="form-group submit">
         <h2>Add a person</h2>
+        <button disabled={disabled}>submit</button>
+        <div className="errors">
+          <div>{errors.name}</div>
+          <div>{errors.email}</div>
+          <div>{errors.password}</div>
+          <div>{errors.terms}</div>
+        </div>
+
+        <div className="form-group inputs">
+          <h4>Important information</h4>
+          <label>
+            Username&nbsp;
+            <input
+              value={values.name}
+              onChange={onChange}
+              name="name"
+              type="text"
+            />
+          </label>
+
+          <label>
+            Email
+            <input
+              value={values.email}
+              onChange={onChange}
+              name="email"
+              type="text"
+            />
+          </label>
+
+          <label>
+            Password
+            <input
+              value={values.password}
+              onChange={onChange}
+              name="password"
+              type="text"
+            />
+          </label>
+
+          <label>
+            Password
+            <input
+              value={values.password}
+              onChange={onChange}
+              name="password"
+              type="text"
+            />
+          </label>
+
+          <label>
+            Terms
+            <input
+              type="checkbox"
+              name="terms"
+              checked={values.terms}
+              onChange={onChange}
+            />
+          </label>
+        </div>
       </div>
     </form>
   )
